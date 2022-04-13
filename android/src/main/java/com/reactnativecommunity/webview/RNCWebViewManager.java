9308d7723e343cb3015f792c1c309881263f5626
Huy Ta <huy@studioanon.co>
2022-04-13 13:55:08 +1000

    change browser agent

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -1270,7 +1270,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
           newWebView.getSettings().setUserAgentString(WebSettings.getDefaultUserAgent(view.getContext()));
         }
       } catch (Exception e) {
-        newWebView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 11; sdk_gphone_arm64 Build/RSR1.210722.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36");
+        newWebView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 12; Pixel 4a (5G)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36");
       }
      
       newWebView.setWebChromeClient(new WebChromeClient() {
